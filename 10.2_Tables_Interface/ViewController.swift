0cee4b798ce403019da150f47888728307369fcc
Alexandre Samson Papa <alekssamson@gmail.com>
2021-05-26 18:56:00 +0200

    Initial Commit

---
+++ 10.2_Tables_Interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10.2_Tables_Interface
+//
+//  Created by Alexandre Samson on 26.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
